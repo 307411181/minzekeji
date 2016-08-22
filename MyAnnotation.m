9405ac2100c2895bcff43f2b6e6809dfe6eca2b8
fanzheng <307411181@qq.com>
2016-08-22 14:49:32 +0800

    no message

---
+++ MyAnnotation.m
@@ -0,0 +1,28 @@
+//
+//  MyAnnotation.m
+//  mapkit
+//
+//  Created by person on 15/8/28.
+//  Copyright (c) 2015年 person. All rights reserved.
+//
+
+#import "MyAnnotation.h"
+
+
+@implementation MyAnnotation
+
+
+
+
+//- (void)initWithImage
+//{
+//    
+//    self.image = [UIImage imageNamed:@"zhuce1"];
+//    
+//}
+
+
+
+
+
+@end
